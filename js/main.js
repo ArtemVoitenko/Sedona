2cd9fb68b2c1418a04e0510b51f590a137efc50f
Artem <artVoitenk072@gmail.com>
2018-07-02 14:29:20 +0300

    restore project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var showNavBtn = document.querySelector(".main-nav__show"),
+    mainNavItem = document.getElementsByClassName("main-nav__item"),
+    hideNavBtn = document.querySelector(".main-nav__hide");
+
+function showNav() { 
+	for (var e = 0; e < mainNavItem.length - 1; e++) 
+		mainNavItem[e].classList.remove("mobile-hidden");
+    hideNavBtn.classList.remove("mobile-hidden") 
+  }
+
+function hideNav() { 
+	for (var e = 0; e < mainNavItem.length - 1; e++) 
+		mainNavItem[e].classList.add("mobile-hidden");
+    this.classList.add("mobile-hidden") } 
+    showNavBtn.addEventListener("click", showNav), 
+    hideNavBtn.addEventListener("click", hideNav);
